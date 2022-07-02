b8394c0c9c5dccdc2c14cb18112d669ffed963e2
miley72 <98891285+miley72@users.noreply.github.com>
2022-07-02 10:24:32 +0100

    Create table.js

---
+++ table.js
@@ -0,0 +1,94 @@
+const employees = {
+    people: {
+        person1: ['1', 'Thompson', 'Anthony', '32', 'Male', 'Head of Finance Department', '17/07/2014'],
+        person2: ['2', 'Johnson', 'Mary', '23', 'Female', 'Software Engineer', '05/03/2019'],
+        person3: ['3', 'Chisom', 'Onyeka', '28', 'Female', 'Human Resource Manager', '21/06/2016'],
+        person4: ['4', 'Jide', 'Olabisi', '29', 'Male', 'Assistant Head(Software Department', '05/06/2016'],
+        person5: ['5', 'WIlliams', 'Ajayi', '30', 'Male', 'Head of Software Department', '06/09/2016'],
+        person6: ['6', 'Nancy', 'Chukwuemeka', '27', 'Female', 'Secretary', '23/08/2016'],
+        person7: ['7', 'Nicholas', 'Uchemba', '26', 'Male', 'Software Engineer', '22/02/2020'],
+        person8: ['8', 'Ahmed', 'Olayinka', '24', 'Male', 'Graphic designer', '15/08/2020'],
+        person9: ['9', 'Halima', 'Mohammed', '26', 'Female', 'Senior Finance Manager', '30/05/2018'],
+        person10: ['10', 'Richard', 'Adeyemi', '41', 'Male', 'CEO', '17/11/2010']
+    }
+}
+
+document.getElementById("a1").innerHTML = employees.people.person1[0]
+document.getElementById("a2").innerHTML = employees.people.person1[1]
+document.getElementById("a3").innerHTML = employees.people.person1[2]
+document.getElementById("a4").innerHTML = employees.people.person1[3]
+document.getElementById("a5").innerHTML = employees.people.person1[4]
+document.getElementById("a6").innerHTML = employees.people.person1[5]
+document.getElementById("a7").innerHTML = employees.people.person1[6]
+
+document.getElementById("b1").innerHTML = employees.people["person2"][0]
+document.getElementById("b2").innerHTML = employees.people["person2"][1]
+document.getElementById("b3").innerHTML = employees.people["person2"][2]
+document.getElementById("b4").innerHTML = employees.people["person2"][3]
+document.getElementById("b5").innerHTML = employees.people["person2"][4]
+document.getElementById("b6").innerHTML = employees.people["person2"][5]
+document.getElementById("b7").innerHTML = employees.people["person2"][6]
+
+document.getElementById("c1").innerHTML = employees.people.person3[0]
+document.getElementById("c2").innerHTML = employees.people.person3[1]
+document.getElementById("c3").innerHTML = employees.people.person3[2]
+document.getElementById("c4").innerHTML = employees.people.person3[3]
+document.getElementById("c5").innerHTML = employees.people.person3[4]
+document.getElementById("c6").innerHTML = employees.people.person3[5]
+document.getElementById("c7").innerHTML = employees.people.person3[6]
+
+document.getElementById("d1").innerHTML = employees.people.person4[0]
+document.getElementById("d2").innerHTML = employees.people.person4[1]
+document.getElementById("d3").innerHTML = employees.people.person4[2]
+document.getElementById("d4").innerHTML = employees.people.person4[3]
+document.getElementById("d5").innerHTML = employees.people.person4[4]
+document.getElementById("d6").innerHTML = employees.people.person4[5]
+document.getElementById("d7").innerHTML = employees.people.person4[6]
+
+document.getElementById("f1").innerHTML = employees.people.person5[0]
+document.getElementById("f2").innerHTML = employees.people.person5[1]
+document.getElementById("f3").innerHTML = employees.people.person5[2]
+document.getElementById("f4").innerHTML = employees.people.person5[3]
+document.getElementById("f5").innerHTML = employees.people.person5[4]
+document.getElementById("f6").innerHTML = employees.people.person5[5]
+document.getElementById("f7").innerHTML = employees.people.person5[6]
+
+document.getElementById("g1").innerHTML = employees.people.person6[0]
+document.getElementById("g2").innerHTML = employees.people.person6[1]
+document.getElementById("g3").innerHTML = employees.people.person6[2]
+document.getElementById("g4").innerHTML = employees.people.person6[3]
+document.getElementById("g5").innerHTML = employees.people.person6[4]
+document.getElementById("g6").innerHTML = employees.people.person6[5]
+document.getElementById("g7").innerHTML = employees.people.person6[6]
+
+document.getElementById("h1").innerHTML = employees.people.person7[0]
+document.getElementById("h2").innerHTML = employees.people.person7[1]
+document.getElementById("h3").innerHTML = employees.people.person7[2]
+document.getElementById("h4").innerHTML = employees.people.person7[3]
+document.getElementById("h5").innerHTML = employees.people.person7[4]
+document.getElementById("h6").innerHTML = employees.people.person7[5]
+document.getElementById("h7").innerHTML = employees.people.person7[6]
+
+document.getElementById("j1").innerHTML = employees.people.person8[0]
+document.getElementById("j2").innerHTML = employees.people.person8[1]
+document.getElementById("j3").innerHTML = employees.people.person8[2]
+document.getElementById("j4").innerHTML = employees.people.person8[3]
+document.getElementById("j5").innerHTML = employees.people.person8[4]
+document.getElementById("j6").innerHTML = employees.people.person8[5]
+document.getElementById("j7").innerHTML = employees.people.person8[6]
+
+document.getElementById("k1").innerHTML = employees.people.person9[0]
+document.getElementById("k2").innerHTML = employees.people.person9[1]
+document.getElementById("k3").innerHTML = employees.people.person9[2]
+document.getElementById("k4").innerHTML = employees.people.person9[3]
+document.getElementById("k5").innerHTML = employees.people.person9[4]
+document.getElementById("k6").innerHTML = employees.people.person9[5]
+document.getElementById("k7").innerHTML = employees.people.person9[6]
+
+document.getElementById("l1").innerHTML = employees.people.person10[0]
+document.getElementById("l2").innerHTML = employees.people.person10[1]
+document.getElementById("l3").innerHTML = employees.people.person10[2]
+document.getElementById("l4").innerHTML = employees.people.person10[3]
+document.getElementById("l5").innerHTML = employees.people.person10[4]
+document.getElementById("l6").innerHTML = employees.people.person10[5]
+document.getElementById("l7").innerHTML = employees.people.person10[6]
